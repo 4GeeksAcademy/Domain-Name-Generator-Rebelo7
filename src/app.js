212582dda3f349f3f4c494ff511cb9b5d940e52e
Rebelo7 <143350138+Rebelo7@users.noreply.github.com>
2023-10-20 18:32:29 +0000

    add a domainNameGenerator

--- src/app.js
+++ src/app.js
@@ -9,3 +9,16 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ["the", "our", "their"];
+let adj = ["great", "big", "gigantic"];
+let noun = ["jogger", "racoon", "toreto"];
+let dom = [".pt", ".com", ".net"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let i = 0; i < adj.length; i++) {
+    for (let i = 0; i < noun.length; i++) {
+      console.log(pronoun[i], adj[i], noun[i], dom[i]);
+    }
+  }
+}
